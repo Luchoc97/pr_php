51891b5225cc4ebe00702188a0d332f0904c4eac
Luchoc97 <luercegue31@gmail.com>
2019-07-07 13:43:46 -0500

    modficar index

--- index.php
+++ index.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>prueba</title>
-        <link href="style.css" rel="stylesheet"/>
-    </head>
-    <body>
 <?php
 echo "hola mundo";
 ?>
-    </body>
-</html>
